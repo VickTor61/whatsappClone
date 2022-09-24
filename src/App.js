f49ad27e0adbd9def02b53d542fed9b674f361ae
Enoma Victor <vickjunior2002@gmail.com>
2022-09-24 20:56:53 +0100

    Add authentication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import nochat from "./img/nochat_background.png";
 import Sidebar from "./sidebar/Sidebar";
 import Chatbar from "./chatbar/Chatbar";
+import Login from "./Login";
 import "./App.css";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="app">
       <div className="app_body">
-        <Router>
-          <Sidebar />
-          <Routes>
-            <Route path="/" element={<h1>Hello</h1>} />
-            <Route
-              path="/rooms/:roomId"
-              element={
-                <>
-                  <Chatbar />
-                </>
-              }
-            />
-          </Routes>
-        </Router>
+        {user ? (
+          <Router>
+            <Sidebar userImage={"fak"} />
+            <Routes>
+              <Route path="/" element={<img src={nochat} alt="no_chat" />} />
+              <Route
+                path="/rooms/:roomId"
+                element={
+                  <>
+                    <Chatbar />
+                  </>
+                }
+              />
+            </Routes>
+          </Router>
+        ) : (
+          <Login />
+        )}
       </div>
     </div>
   );
